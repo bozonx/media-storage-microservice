3ef9331f97a596ea9391a50c529e3abd7aa4ffb1
Ivan <ipkozyrin@gmail.com>
2026-01-16 12:03:20 -0300

    fix

--- src/modules/thumbnails/thumbnail.module.ts
+++ src/modules/thumbnails/thumbnail.module.ts
@@ -3,9 +3,10 @@ import { ThumbnailService } from './thumbnail.service.js';
 import { ThumbnailController } from './thumbnail.controller.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
 import { StorageModule } from '../storage/storage.module.js';
+import { FilesModule } from '../files/files.module.js';
 
 @Module({
-  imports: [PrismaModule, StorageModule],
+  imports: [PrismaModule, StorageModule, FilesModule],
   controllers: [ThumbnailController],
   providers: [ThumbnailService],
   exports: [ThumbnailService],
